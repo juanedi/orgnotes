9f368e7c45b60ffb607d5970cd1cf78d8fa35e52
Juan Edi <jedi11235@gmail.com>
2017-07-08 15:20:24 -0300

    Use "kind" instead of ".tag" in dropbox API driver

--- app/models/dropbox_driver.rb
+++ app/models/dropbox_driver.rb
@@ -8,12 +8,23 @@ class DropboxDriver
     @client.download(file_path)
   end
 
-  # [{ kind: 'directory', name: "..", path_display: '/../..', path_lower: '/../..' }, ..., ...]
   def list_directory(path)
     @client
       .list_folder(path)
       .entries
       .map(&:to_hash)
       .select { |e| e[".tag"] == "folder" || e["name"] =~ /\.org$/ }
+      .map    { |e| api_json(e) }
+  end
+
+  private
+
+  def api_json(entry)
+    {
+      "kind" => entry[".tag"],
+      "name" => entry["name"],
+      "path_display" => entry["path_display"],
+      "path_lower" => entry["path_lower"]
+    }
   end
 end
