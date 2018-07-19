c46be9f256e5e6507f93e0321ceaa191b1a68048
Juan Edi <juan@noredink.com>
2018-07-19 19:37:30 -0300

    Rename object store to "resources"
    We'll store folder contents there too

--- app/assets/javascripts/port_server.js
+++ app/assets/javascripts/port_server.js
@@ -17,6 +17,8 @@
     });
   }
 
+  var RESOURCES_STORE = "resources"
+
   function initializeDb() {
     return new Promise(function(resolve, reject) {
       if (!('indexedDB' in window)) {
@@ -27,8 +29,9 @@
       var openRequest = window.indexedDB.open('orgnotes-db1', 1);
 
       openRequest.onupgradeneeded = function() {
-        if (!this.result.objectStoreNames.contains('notes')) {
-          this.result.createObjectStore('notes', { keyPath: 'path' });;
+        if (!this.result.objectStoreNames.contains(RESOURCES_STORE)) {
+          // TODO: is this operation async?
+          this.result.createObjectStore(RESOURCES_STORE, { keyPath: 'path' });;
         }
       };
 
@@ -43,15 +46,15 @@
   }
 
   function storeNote(request, db) {
-    var tx = db.transaction("notes", "readwrite");
-    var store = tx.objectStore("notes");
+    var tx = db.transaction(RESOURCES_STORE, "readwrite");
+    var store = tx.objectStore(RESOURCES_STORE);
 
     store.put(request.note);
   }
 
   function fetchNote(request, db) {
-    var tx = db.transaction("notes", "readonly");
-    var store = tx.objectStore("notes");
+    var tx = db.transaction(RESOURCES_STORE, "readonly");
+    var store = tx.objectStore(RESOURCES_STORE);
 
     return new Promise(function(resolve, reject) {
       var getRequest = store.get(request.path);
